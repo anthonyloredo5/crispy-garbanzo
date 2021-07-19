02b138a8ddc082c1074d0127a0fdd86a32fec986
Anthony Loredo <anthonyloredo5@gmail.com>
2021-07-19 08:37:33 -0400

    Isolated array of emails

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import getEmails from './api/API';
+
 
 import BasicTable from './components/table';
 
 function App() {
   console.log("App running");
-  getEmails();
-  console.log("Should log emails");
 
   return (
     <div className="App">
--- src/api/API.js
+++ src/api/API.js
@@ -1,11 +1,16 @@
 import axios from 'axios';
 
 const getEmails = () => {
+    var response;
     axios.get('https://api.hunter.io/v2/domain-search?domain=stripe.com&api_key=70e5cc02afc8eb951522b63e51de10edfe5abe4f')
     .then(response => {
-        console.log(response);
-        return response;
+        console.log(response, "response");
+        console.log(response.data.data, "response.data.data");
+        console.log(response.data.data.emails, "response.data.data.emails");
+        response = response.data.data.emails;
     })
+    console.log(response, " should be response ");
+    return response;
 }
 
 export default getEmails;
